e410f8b96699ef26496c02a0c895302464b4e190
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-10-10 15:16:31 +0300

    fix bag(remove directory)

--- src/main/java/com/bogdan/aeroreserve/controller/StatisticsController.java
+++ src/main/java/com/bogdan/aeroreserve/controller/StatisticsController.java
@@ -35,7 +35,7 @@ public class StatisticsController {
                 .orElseThrow(() -> new RuntimeException("User not found"));
         model.addAttribute("stats", statisticsService.getOrCreateStatistics(flightId));
         model.addAttribute("user", user);
-        return "flight/flight-statistics";
+        return "flight-statistics";
     }
 
     @GetMapping("/statistics/refresh")
