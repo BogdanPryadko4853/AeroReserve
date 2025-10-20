274f469f850cfffd31c8437ce4e2c9c51549e58b
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-10-21 00:18:55 +0300

    replace to css and fix security config(css downloading)

--- src/main/java/com/bogdan/aeroreserve/config/SecurityConfig.java
+++ src/main/java/com/bogdan/aeroreserve/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bogdan.aeroreserve.config;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/", "/css/**", "/js/**", "/register", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
